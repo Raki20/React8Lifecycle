02f36fe33fe7e286c228baa20a09475ecee07b89
Raquel Moreira <araquelmoreira@gmail.com>
2019-05-24 10:59:14 +0100

    start repo

---
+++ src/Form.jsx
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+
+class Form extends Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            title:'',
+            globalTitle:'Title this shit',
+        };
+    }
+
+    componentDidMount() {
+        console.log('Rendered Form');
+    }
+  
+    componentDidUpdate() {
+        console.log('Title changed');
+        }
+ 
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        const { title } = this.state;
+        this.setState({ 
+            globalTitle: `My form - ${title}`,
+            title: '',
+        });
+    }
+
+    handleChange = (e) => {
+        this.setState({ title: e.target.value });
+    }
+
+    render() {
+        const { title, globalTitle } = this.state;
+  return (
+    <div>
+        <h1>{globalTitle}</h1>
+
+      <form onSubmit={this.handleSubmit}>
+        <label>
+            <input type="text" name="" value={title} onChange={this.handleChange} />
+        </label>
+            <input type="submit" value="Submit" name="" />
+        </form>
+    </div>
+  );
+  }
+  }
+  
+  export default Form;
+  
